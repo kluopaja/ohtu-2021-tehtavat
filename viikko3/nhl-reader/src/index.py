8b3e4176af901d043bcdb3895692bc8e85aa8353
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-02-19 19:09:21 +0200

    Add more attributes to Player class
    	Modify __repr__ of Player
    	Modify index.py accordingly

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -5,11 +5,22 @@ import requests
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     response = requests.get(url).json()
+    print(response[0])
+    pass
     
     players = []
-    for player_dicts in response:
-        players.append(Player(player_dicts['name']))
+    for player_dict in response:
+        new_player = Player(name=player_dict['name'],
+                            nationality=player_dict['nationality'],
+                            assists=player_dict['assists'],
+                            goals=player_dict['goals'],
+                            penalties=player_dict['penalties'],
+                            team=player_dict['team'],
+                            games=player_dict['games'])
+        players.append(new_player)
 
-    print(players)
+    
+    print(players[0])
+
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,10 +1,22 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
     
     def __str__(self):
-        return self.name
+        return self.__repr__()
 
     def __repr__(self):
-        return f'Player(self.name)'
+        return f"Player(name='{self.name}', \
+nationality='{self.nationality}', \
+assists='{self.assists}', \
+goals='{self.goals}', \
+penalties='{self.penalties}', \
+team='{self.team}', \
+games='{self.games}')"
 
