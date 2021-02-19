d9b601c7f6a947f5ccce646c16b3509a7ee0beb3
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-02-19 17:30:44 +0200

    Add JSON query
    	Query simple player details and print them
    
    	Add __repr__ to Player class

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
 from player import Player
+import requests
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+    
+    players = []
+    for player_dicts in response:
+        players.append(Player(player_dicts['name']))
 
+    print(players)
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -4,3 +4,7 @@ class Player:
     
     def __str__(self):
         return self.name
+
+    def __repr__(self):
+        return f'Player(self.name)'
+
