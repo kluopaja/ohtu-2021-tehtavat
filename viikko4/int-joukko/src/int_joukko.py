fb36b592f0d15ec038d19872d700d452085f9982
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-05 01:25:46 +0300

    Refactor IntJoukko
        Refactor IntJoukko.kuuluu

--- viikko4/int-joukko/src/int_joukko.py
+++ viikko4/int-joukko/src/int_joukko.py
@@ -23,16 +23,7 @@ class IntJoukko:
         self.alkioiden_lkm = 0
 
     def kuuluu(self, n):
-        on = 0
-
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                on = on + 1
-
-        if on > 0:
-            return True
-        else:
-            return False
+        return n in self.ljono[0:self.alkioiden_lkm]
 
     def lisaa(self, n):
         ei_ole = 0
