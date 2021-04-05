7b1da38d07d41da2fdde9d214a1efe7395d97e7e
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-05 03:30:10 +0300

    Refactor IntJoukko
        Remove/improve comments
    
        Remove redundant code
    
        Simplify expressions

--- viikko4/int-joukko/src/int_joukko.py
+++ viikko4/int-joukko/src/int_joukko.py
@@ -7,14 +7,14 @@ class IntJoukko:
         if kapasiteetti is None:
             self.kapasiteetti = KAPASITEETTI
         elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("Väärä kapasiteetti")  # heitin vaan jotain :D
+            raise Exception("Väärä kapasiteetti")
         else:
             self.kapasiteetti = kapasiteetti
 
         if kasvatuskoko is None:
             self.kasvatuskoko = OLETUSKASVATUS
         elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("kapasiteetti2")  # heitin vaan jotain :D
+            raise Exception("kapasiteetti2")
         else:
             self.kasvatuskoko = kasvatuskoko
 
@@ -26,18 +26,14 @@ class IntJoukko:
         return n in self.ljono[0:self.alkioiden_lkm]
 
     def lisaa(self, n):
-        ei_ole = 0
-
         if self.alkioiden_lkm == 0:
             self.ljono[0] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
+            self.alkioiden_lkm += 1
             return True
-        else:
-            pass
 
         if not self.kuuluu(n):
             self.ljono[self.alkioiden_lkm] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
+            self.alkioiden_lkm += 1
 
             if self.alkioiden_lkm % len(self.ljono) == 0:
                 taulukko_old = self.ljono
@@ -51,27 +47,23 @@ class IntJoukko:
 
     def poista(self, n):
         kohta = -1
-        apu = 0
 
-        for i in range(0, self.alkioiden_lkm):
+        for i in range(self.alkioiden_lkm):
             if n == self.ljono[i]:
-                kohta = i  # siis luku löytyy tuosta kohdasta :D
-                self.ljono[kohta] = 0
+                kohta = i  # haluttu luku löytyy tuosta kohdasta
                 break
 
         if kohta != -1:
             for j in range(kohta, self.alkioiden_lkm - 1):
-                apu = self.ljono[j]
                 self.ljono[j] = self.ljono[j + 1]
-                self.ljono[j + 1] = apu
 
-            self.alkioiden_lkm = self.alkioiden_lkm - 1
+            self.alkioiden_lkm -= 1
             return True
 
         return False
 
     def kopioi_taulukko(self, a, b):
-        for i in range(0, len(a)):
+        for i in range(len(a)):
             b[i] = a[i]
 
     def mahtavuus(self):
@@ -80,7 +72,7 @@ class IntJoukko:
     def to_int_list(self):
         taulu = [0] * self.alkioiden_lkm
 
-        for i in range(0, len(taulu)):
+        for i in range(len(taulu)):
             taulu[i] = self.ljono[i]
 
         return taulu
@@ -91,10 +83,10 @@ class IntJoukko:
         a_taulu = a.to_int_list()
         b_taulu = b.to_int_list()
 
-        for i in range(0, len(a_taulu)):
+        for i in range(len(a_taulu)):
             x.lisaa(a_taulu[i])
 
-        for i in range(0, len(b_taulu)):
+        for i in range(len(b_taulu)):
             x.lisaa(b_taulu[i])
 
         return x
@@ -105,8 +97,8 @@ class IntJoukko:
         a_taulu = a.to_int_list()
         b_taulu = b.to_int_list()
 
-        for i in range(0, len(a_taulu)):
-            for j in range(0, len(b_taulu)):
+        for i in range(len(a_taulu)):
+            for j in range(len(b_taulu)):
                 if a_taulu[i] == b_taulu[j]:
                     y.lisaa(b_taulu[j])
 
