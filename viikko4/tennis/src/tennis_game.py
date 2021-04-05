ca3df8d791d714c7ce27806406ec2c6585f32994
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-05 21:12:44 +0300

    Refactor TennisGame
        Replace magic constants with variables

--- viikko4/tennis/src/tennis_game.py
+++ viikko4/tennis/src/tennis_game.py
@@ -5,6 +5,11 @@ class TennisGame:
         self.player1_points = 0
         self.player2_points = 0
         self.simple_scores = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+        self.tie = "Deuce"
+        self.advantage = "Advantage"
+        self.win = "Win for"
+        self.all = "All"
+        self.end_game_point_threshold = 4
 
     def won_point(self, player_name):
         if player_name == self.player1_name:
@@ -19,24 +24,24 @@ class TennisGame:
             return self._get_early_game_score()
 
     def _in_end_game(self):
-        return max(self.player1_points, self.player2_points) >= 4
+        return max(self.player1_points, self.player2_points) >= self.end_game_point_threshold
 
     def _get_end_game_score(self):
         point_difference = self.player1_points - self.player2_points
         if point_difference == 0:
-            return "Deuce"
+            return self.tie
         elif point_difference == 1:
-            return "Advantage " + self.player1_name
+            return self.advantage + " " + self.player1_name
         elif point_difference == -1:
-            return "Advantage " + self.player2_name
+            return self.advantage + " " + self.player2_name
         elif point_difference >= 2:
-            return "Win for " + self.player1_name
+            return self.win + " " + self.player1_name
         else:
-            return "Win for " + self.player2_name
+            return self.win + " " + self.player2_name
 
     def _get_early_game_score(self):
         if self.player1_points == self.player2_points:
-            return self.simple_scores[self.player1_points]  + "-All"
+            return self.simple_scores[self.player1_points]  + "-" + self.all
         else:
             return self.simple_scores[self.player1_points] + "-"\
                    + self.simple_scores[self.player2_points]
