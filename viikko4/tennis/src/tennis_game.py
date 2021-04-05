6243a9af21f3aa322659fb00c3952f62b55a2496
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-05 22:34:34 +0300

    Refactor TennisGame
        Refactor printing end game score
    
        Make separators into variables

--- viikko4/tennis/src/tennis_game.py
+++ viikko4/tennis/src/tennis_game.py
@@ -10,6 +10,8 @@ class TennisGame:
         self.win = "Win for"
         self.all = "All"
         self.end_game_point_threshold = 4
+        self.endgame_score_separator = " "
+        self.early_game_score_separator = "-"
 
     def won_point(self, player_name):
         if player_name == self.player1_name:
@@ -20,28 +22,27 @@ class TennisGame:
     def get_score(self):
         if self._in_end_game():
             return self._get_end_game_score()
-        else:
-            return self._get_early_game_score()
+        return self._get_early_game_score()
 
     def _in_end_game(self):
         return max(self.player1_points, self.player2_points) >= self.end_game_point_threshold
 
     def _get_end_game_score(self):
-        point_difference = self.player1_points - self.player2_points
-        if point_difference == 0:
+        absolute_point_difference = abs(self.player1_points - self.player2_points)
+        if absolute_point_difference == 0:
             return self.tie
-        elif point_difference == 1:
-            return self.advantage + " " + self.player1_name
-        elif point_difference == -1:
-            return self.advantage + " " + self.player2_name
-        elif point_difference >= 2:
-            return self.win + " " + self.player1_name
-        else:
-            return self.win + " " + self.player2_name
+        if absolute_point_difference == 1:
+            return self.advantage + self.endgame_score_separator + self._get_leading_player_name()
+        return self.win + self.endgame_score_separator + self._get_leading_player_name()
+
+    def _get_leading_player_name(self):
+        if self.player1_points > self.player2_points:
+            return self.player1_name
+        return self.player2_name
 
     def _get_early_game_score(self):
         if self.player1_points == self.player2_points:
-            return self.simple_scores[self.player1_points]  + "-" + self.all
+            return self.simple_scores[self.player1_points]  + self.early_game_score_separator + self.all
         else:
-            return self.simple_scores[self.player1_points] + "-"\
+            return self.simple_scores[self.player1_points] + self.early_game_score_separator \
                    + self.simple_scores[self.player2_points]
