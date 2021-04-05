6cd1de7a4a19dc667fe620fa725ade9c15175101
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-05 16:35:07 +0300

    Refactor TennisGame
        Rename variables
    
        Remove an extra whitespace

--- viikko4/tennis/src/tennis_game.py
+++ viikko4/tennis/src/tennis_game.py
@@ -2,48 +2,48 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_points = 0
+        self.player2_points = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player1_points = self.player1_points + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_points = self.player2_points + 1
 
     def get_score(self):
         score = ""
         temp_score = 0
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
+        if self.player1_points == self.player2_points:
+            if self.player1_points == 0:
                 score = "Love-All"
-            elif self.m_score1 == 1:
+            elif self.player1_points == 1:
                 score = "Fifteen-All"
-            elif self.m_score1 == 2:
+            elif self.player1_points == 2:
                 score = "Thirty-All"
-            elif self.m_score1 == 3:
+            elif self.player1_points == 3:
                 score = "Forty-All"
             else:
                 score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        elif self.player1_points >= 4 or self.player2_points >= 4:
+            point_difference = self.player1_points - self.player2_points
 
-            if minus_result == 1:
+            if point_difference == 1:
                 score = "Advantage player1"
-            elif minus_result == -1:
+            elif point_difference == -1:
                 score = "Advantage player2"
-            elif minus_result >= 2:
+            elif point_difference >= 2:
                 score = "Win for player1"
             else:
                 score = "Win for player2"
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.m_score1
+                    temp_score = self.player1_points
                 else:
                     score = score + "-"
-                    temp_score = self.m_score2
+                    temp_score = self.player2_points
 
                 if temp_score == 0:
                     score = score + "Love"
